fbc7dc3ea90fcfdc496214ef350328774e64839d
yashu <yashaswinicv5@gmail.com>
2026-03-18 21:38:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: simple greeting in console
+console.log("Welcome to my portfolio!");
+
+// You can later add interactive features like:
+// - Dark mode toggle
+// - Project filtering
+// - Contact form validation
